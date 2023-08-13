ea604d5b672dcbd570771e293ed3d0550351bc83
Santiago <santiagocortondo25@gmail.com>
2023-08-13 15:35:57 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var div = document.getElementById("div");
-    div.addEventListener("click", function() {
-      alert("Hola! Soy el div");
-    });
+  var div = document.getElementById("div");
+  var boton = document.getElementById("boton");
+
+  div.addEventListener("click", function() {
+    alert("Hola! Soy el div");
   });
+
+  boton.addEventListener("click", function(event) {
+    event.stopPropagation();
+    alert("Hola!");
+  });
+});
   
 
   
