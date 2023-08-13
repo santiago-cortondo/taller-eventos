2b2456e6c424213c779f6689e6fe63082a4e0ff5
Santiago <santiagocortondo25@gmail.com>
2023-08-13 14:58:59 -0300

    addEventListener

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var div = document.getElementById("div");
+    div.addEventListener("click", function() {
+      alert("Hola! Soy el div");
+    });
+  });
+  
+
+  
